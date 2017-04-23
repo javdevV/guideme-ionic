a86275d6849fe3ffeb79af34b4c08ae78cd921ce
TaiebAlFalah <mohamedtaieb.alfalah@esprit.tn>
2017-04-23 06:49:03 +0100

    setting the weather

---
+++ src/pages/weather/weather.ts
@@ -0,0 +1,9 @@
+import { Weather } from './../../providers/weather';
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+
+@Component({
+  selector: 'page-weather',
+  templateUrl: 'weather.html'
+})
