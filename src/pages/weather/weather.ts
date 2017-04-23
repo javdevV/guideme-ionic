35e5dac33eee032e7ce1c1dc8cce232220817035
TaiebAlFalah <mohamedtaieb.alfalah@esprit.tn>
2017-04-23 06:51:47 +0100

    0.0.2

--- src/pages/weather/weather.ts
+++ src/pages/weather/weather.ts
@@ -7,3 +7,17 @@ import { Storage } from '@ionic/storage';
   selector: 'page-weather',
   templateUrl: 'weather.html'
 })
+export class WeatherPage {
+
+  public zmw: string; //elle fait référence lel city el ZMW  ¯\_(ツ)_/¯ 
+  public weather: any;
+  public searchStr: string;
+  public results: any;
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    private weatherService: Weather,
+    private storage: Storage) {
+      
+    }
