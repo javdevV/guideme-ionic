e50487b1e6524397d29e73744280ea6f2bf93099
TaiebAlFalah <mohamedtaieb.alfalah@esprit.tn>
2017-04-23 06:47:49 +0100

    setting up the weather api with some deep informations

--- src/providers/weather.ts
+++ src/providers/weather.ts
@@ -7,4 +7,14 @@ export class Weather {
   public apiKey: string = '';
   public conditionsUrl: string = '';
   public searchUrl: string;
-  public alertUrl: string ='';}
+  public alertUrl: string ='';
+    //😀😀😀😀😀😀😀😀😀😀😀😀😀DOCUMENTATION EMTA3 EL WEATHER😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀
+//https://www.wunderground.com/weather/api/d/docs
+//😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀v
+  constructor(private _http: Http) {
+    this.apiKey = 'bc9e6b2f5532265e';
+    this.conditionsUrl = 'http://api.wunderground.com/api/'+ this.apiKey +'/conditions/q';
+    this.alertUrl= 'http://api.wunderground.com/api/'+ this.apiKey +'/alerts/q';
+    //https://nihongo.wunderground.com/weather/api/d/docs?d=autocomplete-api Documentation emta3 el autocomplete
+    this.searchUrl = 'http://autocomplete.wunderground.com/aq?query=';
+  }
